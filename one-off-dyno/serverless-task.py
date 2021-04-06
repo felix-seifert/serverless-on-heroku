dc2c62b4e655433f2a5e68b3a8bb81960a7e60d6
Felix Seifert <mail@felix-seifert.com>
2021-04-06 19:41:01 +0200

    Simplify Python Script

--- one-off-dyno/serverless-task.py
+++ one-off-dyno/serverless-task.py
@@ -2,6 +2,7 @@ from os import environ
 
 if 'NAME' in environ and len(environ['NAME'].strip()) > 0:
     name = environ['NAME'].strip()
-    print('Hello ' + name + '!')
 else:
-    print('Hello World!')
+    name = 'World'
+
+print('Hello ' + name + '!')
